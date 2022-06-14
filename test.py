f2d08ce2700b22b86a84ec3afa62e396e2d96f08
prestontranbarger <preston.tranbarger@gmail.com>
2022-06-13 20:40:42 -0500

    almost a breakthrough

--- test.py
+++ test.py
@@ -2,3 +2,17 @@ from additiveCharacters import *
 from dedekindSum import *
 from dirichletCharacters import *
 from SLTwoZ import *
+
+def sortKey(m):
+    return m[1][0]
+
+for n in range(3, 25):
+    gens = sorted(list(set(Gamma0(n).generators(algorithm="todd-coxeter"))), key = sortKey)
+    if n in Primes():
+        print(n, len(gens))
+        cFs = []
+        for gen in gens:
+            cFs.append(contFrac(gen[0][0], gen[1][0]))
+        sCFs = sorted(cFs[4:], key = len)
+        for cF in sCFs:
+            print(cF)
---
+++ test2.py
@@ -0,0 +1,7 @@
+from additiveCharacters import *
+from dedekindSum import *
+from dirichletCharacters import *
+from SLTwoZ import *
+
+print(contFrac(1, 3))
+print(buildMatrix(1, 3))
